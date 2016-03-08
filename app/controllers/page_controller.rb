b2902237f54f5a532d12e5f60dcd3cf95a4fc160
quakex <iquakex@gmail.com>
2016-03-09 06:38:42 +0800

    增加数据库支持

--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -1,7 +1,7 @@
 class PageController < ApplicationController
 
   def welcome
-    @issues = [{title: "Ruby lovers", comments: "4"}, {title:"Rails and Laravel", comments:"5"}]
+    @issues = Issue.all
 
   end
 end
