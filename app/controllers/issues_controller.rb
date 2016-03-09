cb394f3b1cac4a0fb5eefc2bb4120be044acd7f6
quakex <iquakex@gmail.com>
2016-03-09 15:43:22 +0800

    form，网站的耳朵

--- app/controllers/issues_controller.rb
+++ app/controllers/issues_controller.rb
@@ -9,4 +9,19 @@ class IssuesController < ApplicationController
     issue.destroy
     redirect_to :root
   end
+
+  def new
+    @issue = Issue.new
+  end
+
+  def create
+    Issue.create(issue_params)
+    redirect_to :root
+  end
+
+  private
+    def issue_params
+      params.require(:issue).permit(:title, :content)
+    end
+
 end
